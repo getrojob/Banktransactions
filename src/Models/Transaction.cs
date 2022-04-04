1797854861ae10be1ce6f37b0672b7490909cc77
getrojob <getrojob@gmai.com>
2022-04-04 08:39:17 -0300

    create Transaction Class

---
+++ src/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace BankTransactions.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public string AccountNumber { get; set; }
+        public string BeneficiaryName { get; set; }
+        public string BankName { get; set; }
+        public string SWIFTCode { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
