6127198b422df51164228ba7df0bf2ffc0d43abb
getrojob <getrojob@gmai.com>
2022-04-05 22:05:32 -0300

    Edit AddOrEdit na classe e na controller

--- src/Models/Transaction.cs
+++ src/Models/Transaction.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BankTransactions.Models
@@ -9,16 +10,26 @@ namespace BankTransactions.Models
         public int TransactionId { get; set; }
 
         [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [Required]
         public string AccountNumber { get; set; }
 
+        [DisplayName("Beneficiary Name")]
         [Column(TypeName = "nvarchar(100)")]
+        [Required]
         public string BeneficiaryName { get; set; }
 
+        [DisplayName("Bank Name")]
         [Column(TypeName = "nvarchar(100)")]
+        [Required]
         public string BankName { get; set; }
 
+        [DisplayName("SWIFT Code")]
         [Column(TypeName = "nvarchar(11)")]
+        [Required]
         public string SWIFTCode { get; set; }
+
+        [Required]
         public int Amount { get; set; }
         public DateTime Date { get; set; }
     }
